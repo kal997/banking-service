bc4f5d54cb6c8141dbd3a139cd5abdade79c69c3
Khaled Soliman <khaled.soliman97@gmail.com>
2025-04-23 01:09:22 +0200

    adding transaction validate method state tests

---
+++ dto/transaction_test.go
@@ -0,0 +1,44 @@
+package dto
+
+import (
+	"net/http"
+	"testing"
+)
+
+func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawal(t *testing.T) {
+	//AAA
+
+	// Arrange
+	transaction := TransactionRequest{Amount: 50, TransactionType: "invalid"}
+
+	// Act
+	appErr := transaction.Validate()
+
+	// Assert
+	if appErr.Message != "Transaction type can only be deposit or withdrawal" {
+		t.Error("Invalid message while testing transaction type")
+	}
+
+	if appErr.Code != http.StatusUnprocessableEntity {
+		t.Error("Invalid code while testing transaction type")
+	}
+}
+
+func Test_should_return_error_when_amount_is_less_than_zero(t *testing.T) {
+	//AAA
+
+	// Arrange
+	transaction := TransactionRequest{Amount: -5, TransactionType: "deposit"}
+
+	// Act
+	appErr := transaction.Validate()
+
+	// Assert
+	if appErr.Message != "Amount cannot be less than zero" {
+		t.Error("Invalid message while validating amount")
+	}
+
+	if appErr.Code != http.StatusUnprocessableEntity {
+		t.Error("Invalid code while validating amount")
+	}
+}
