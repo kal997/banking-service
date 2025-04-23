941e9efab75832a6332ab69beba4ff8de76af221
Khaled Soliman <khaled.soliman97@gmail.com>
2025-04-24 00:02:18 +0200

    refactors CustomerService mock and tests GetCustomers endpoint's positive and negative scenarios

--- app/customerHandlers_test.go
+++ app/customerHandlers_test.go
@@ -15,21 +15,30 @@ import (
 // testing getallcustomers as a unit, we will inject the mock (the only external dependency)
 // and we expect a slice of customers if the request is valid, and an error one if not
 
+var mockService *service.MockCustomerService
+var ch CustomerHandler
+var router *mux.Router
+
+func setup(t *testing.T) func() {
+	ctrl := gomock.NewController(t)
+	mockService = service.NewMockCustomerService(ctrl)
+
+	ch = CustomerHandler{mockService}
+	router = mux.NewRouter()
+	router.HandleFunc("/customers", ch.GetAllCustomers)
+	return func() {
+		router = nil
+		defer ctrl.Finish()
+	}
+
+}
+
 func Test_should_return_customer_with_status_code_200(t *testing.T) {
 	//AAA
 
 	//Arange (mock setup)
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mockService := service.NewMockCustomerService(ctrl)
-
-	ch := CustomerHandler{mockService}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/customers", ch.GetAllCustomers)
-
-	// untill now, what we have done is creating a new router with one route, and the handler of this route will use the
-	// mock service instead of the default one
+	teardown := setup(t)
+	defer teardown()
 
 	// what is needed, is to implement the behaviour we want for positive scenario test, i.e. returning a slice of customers from the mock
 	dummyCustomers := []dto.CustomerResponse{
@@ -55,25 +64,12 @@ func Test_should_return_status_code_500_with_error_message(t *testing.T) {
 	//AAA
 
 	//Arange (mock setup)
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mockService := service.NewMockCustomerService(ctrl)
-
-	ch := CustomerHandler{mockService}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/customers", ch.GetAllCustomers)
-
-	// untill now, what we have done is creating a new router with one route, and the handler of this route will use the
-	// mock service instead of the default one
-
-	// what is needed, is to implement the behaviour we want for positive scenario test, i.e. returning a slice of customers from the mock
-
+	teardown := setup(t)
+	defer teardown()
 	mockService.EXPECT().GetAllCustomers("").Return(nil, errs.NewUnexpectedError("some db errors"))
 	request, _ := http.NewRequest(http.MethodGet, "/customers", nil)
 
 	// Act (create a http request)
-	// since we need a response writer for the test, not a normal one
 	recorder := httptest.NewRecorder()
 	router.ServeHTTP(recorder, request) // test recorder will bring the response from the handler
 
